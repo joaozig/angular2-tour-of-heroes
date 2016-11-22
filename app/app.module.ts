018e1ac6eb3f9283b9ea5f45d85ba8005b3b36e4
João Ricardo Bastos <joaoricardobastos@gmail.com>
2016-11-21 22:37:31 -0300

    Makes the route available

--- app/app.module.ts
+++ app/app.module.ts
@@ -8,16 +8,22 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
 
-RouterModule.forRoot([
-    {
-        path: 'heroes',
-        component: HeroesComponent
-    }
-])
-
 @NgModule({
-    imports: [BrowserModule, FormsModule],
-    declarations: [AppComponent, HeroDetailComponent, HeroesComponent],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        RouterModule.forRoot([
+            {
+                path: 'heroes',
+                component: HeroesComponent
+            }
+        ])
+    ],
+    declarations: [
+        AppComponent, 
+        HeroDetailComponent, 
+        HeroesComponent
+    ],
     providers: [HeroService],
     bootstrap: [AppComponent]
 })
