b37f45a2498356c4fbc1880dea642d32ccb2d447
João Ricardo Bastos <joaoricardobastos@gmail.com>
2016-11-21 22:18:23 -0300

    Moves AppComponent to HeroesComponent

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -4,7 +4,7 @@ import { Hero }        from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
-    selector: 'my-app',
+    selector: 'my-heroes',
     providers: [HeroService],
     template:`
         <h1>{{title}}</h1>
@@ -68,7 +68,7 @@ import { HeroService } from './hero.service';
         }
     `]
 })
-export class AppComponent implements OnInit {
+export class HeroesComponent implements OnInit {
     title = 'Tour of Heroes';
     heroes: Hero[];
     selectedHero: Hero;
