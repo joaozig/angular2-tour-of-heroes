d7662bd2daf5744afba4a65fa31120fac4c04742
João Ricardo Bastos <joaoricardobastos@gmail.com>
2016-11-21 22:32:07 -0300

    Setups the initial route configuration

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }  from '@angular/router';
 
 import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
+
+RouterModule.forRoot([
+    {
+        path: 'heroes',
+        component: HeroesComponent
+    }
+])
 
 @NgModule({
     imports: [BrowserModule, FormsModule],
