8abbd327d7bfffe54676169e9162df26bf4fd6e2
João Ricardo Bastos <joaoricardobastos@gmail.com>
2016-11-22 23:02:09 -0300

    Refactors AppModule by adding AppRoutingModule to handle routes

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,7 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { RouterModule }  from '@angular/router';
+import { NgModule }         from '@angular/core';
+import { BrowserModule }    from '@angular/platform-browser';
+import { FormsModule }      from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -13,25 +13,7 @@ import { DashboardComponent }  from './dashboard.component';
     imports: [
         BrowserModule,
         FormsModule,
-        RouterModule.forRoot([
-            {
-                path: '',
-                redirectTo: '/dashboard',
-                pathMatch: 'full'
-            },
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            },
-            {
-                path: 'heroes',
-                component: HeroesComponent
-            },
-            {
-                path: 'detail/:id',
-                component: HeroDetailComponent
-            }
-        ])
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
