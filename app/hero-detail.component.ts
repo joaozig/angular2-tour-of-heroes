37d8dc424f7e9ab8819c2ec9e9643c834e68cfa8
João Ricardo Bastos <joaoricardobastos@gmail.com>
2016-11-23 22:50:41 -0300

    Changes HeroesComponent to show a mini-detail

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -4,6 +4,7 @@ import { Location }                 from '@angular/common';
 import 'rxjs/add/operator/switchMap'
 
 import { HeroService } from './hero.service';
+import { Hero }        from './hero';
 
 @Component({
     moduleId: module.id,
